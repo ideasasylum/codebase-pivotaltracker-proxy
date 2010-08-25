121bbc9b8c5937634d38639f57a0b53b01df75a0
Jamie Lawrence <hopeless@gmail.com>
2010-08-25 01:08:35 +0100

    fixed a few missing gems and variables

--- proxy.rb
+++ proxy.rb
@@ -1,21 +1,25 @@
 require 'rubygems'
 require 'sinatra'
-require 'curl'
+require 'curb'
 require 'xml/xslt'
+require 'haml'
+
+set :run, true
 
 get '/' do
     haml :index
 end
 
-post '/tickets/:server/:username/:apikey/:project' do
+get '/tickets/:server/:username/:apikey/:project' do
     # Get user's posterous details
     apikey = params[:apikey]
-    user = params[:username]
+    username = params[:username]
     project = params[:project]
+    server = params[:server]
     
     c = Curl::Easy.new("http://#{server}.codebasehq.com/#{project}/tickets.xml") 
     c.username = username
-    c.password = password
+    c.password = apikey
     c.http_get
     tickets = c.body_str
     puts tickets
@@ -27,3 +31,18 @@ post '/tickets/:server/:username/:apikey/:project' do
 	out = xslt.serve()
     puts outcb-pt.xslt
 end
+
+enable :inline_templates
+
+__END__
+
+@@ layout
+%html
+  = yield
+
+
+@@ index
+%h1 Codebase 2 PivotalTracker
+%p 
+  A tiny proxy application which will extract your tickets from Codebase and allow them to be viewed in PivotalTracker. Written by
+  %a(href="http://ideasasylum.com") Jamie Lawrence and hosted on Heroku. 
