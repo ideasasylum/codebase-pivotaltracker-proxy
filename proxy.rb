d634c7a3a25bf652efc3dd7b0b082a227f7bca61
Jamie Lawrence <hopeless@gmail.com>
2010-08-25 01:15:36 +0100

    nlined xslt

--- proxy.rb
+++ proxy.rb
@@ -26,10 +26,28 @@ get '/tickets/:server/:username/:apikey/:project' do
     
 	xslt = XML::XSLT.new()
 	xslt.xml = tickets
-	xslt.xsl = "cb2pt.xslt"
+	xslt.xsl = <<XML
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:template match="/tickets">
+  <external_stories type="array">
+      <xsl:for-each select="ticket">
+      <external_story>
+        <external_id><xsl:value-of select="ticket-id"/></external_id>
+        <description><xsl:value-of select="summary"/></description>
+        <name><xsl:value-of select="summary"/></name>
+        <requested_by><xsl:value-of select="reporter"/></requested_by>
+        <story_type><xsl:value-of select="ticket-type"/></story_type>
+      </external_story>
+      </xsl:for-each>
+  </external_stories>
+</xsl:template>
+</xsl:stylesheet>
+XML
 
 	out = xslt.serve()
-    puts outcb-pt.xslt
+    puts out
 end
 
 enable :inline_templates
